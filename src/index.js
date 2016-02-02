da3320865dc2a774dfde6840fe6f57eb321d124f
Samuel Loup <loup@epsitec.ch>
2016-02-02 09:20:42 +0100

    Add missing deps, and enable touche-tap for react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 'use strict';
 import { IntlProvider } from 'react-intl';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin ();
+
 const curry = require ('ramda').curry;
 const merge = require ('ramda').merge;
 
