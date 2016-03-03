265e0cd915dacf67662c5fb60e58072b29925631
Samuel Loup <loup@epsitec.ch>
2016-03-03 20:29:42 +0100

    Impl. T func, for translating text in your codebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const curry = require ('ramda').curry;
 const merge = require ('ramda').merge;
-
+const formatMessage = require('format-message');
 // Base action creator
 const createCommand = (command) => ({type: command});
 const setPayload = (action, payload) => merge (action, payload);
@@ -30,6 +30,13 @@ const addMessage = (m, d) => createAction ('NABU_ADD_MESSAGE', {
 const toggleMarker = () => createAction ('NABU_TOGGLE_MARKS', {});
 const toggleTranslatorPanel = () => createAction ('NABU_TOGGLE_TRANSLATOR', {});
 
+const mustTranslate = (messages, msgid) => {
+  const mustTranslate = !messages.has (msgid);
+  if (mustTranslate) {
+    return mustTranslate;
+  }
+  return !messages.getIn ([msgid, 'translated']);
+};
 // API
 module.exports = {
   changeLocale: changeLocale,
@@ -38,5 +45,28 @@ module.exports = {
   toggleMarker: toggleMarker,
   toggleTranslatorPanel: toggleTranslatorPanel,
   initialState: require ('./initialState.js'),
-  nabuReducer: require ('./reducer.js')
+  nabuReducer: require ('./reducer.js'),
+  T: (store) => {
+    return (msgid, values, desc) => {
+      const state = store.getState ();
+      const messages = state.nabu.get (state.nabu.get ('locale'));
+      const mustAdd = !messages.has (msgid);
+      if (mustAdd) {
+        console.log ('NABU_ADD_MESSAGE:', msgid);
+        store.dispatch (addMessage (msgid, desc));
+      }
+      const marker = state.nabu.get ('marker');
+      const fallbackMessage = {
+          id: msgid,
+          defaultMessage: msgid,
+          description: desc
+      };
+      const markerOn = mustTranslate (messages, msgid) && marker;
+      let text = formatMessage (messages.get (msgid, fallbackMessage), values);
+      if (markerOn) {
+        text = '#' + text + '#';
+      }
+      return text;
+    };
+  }
 };
