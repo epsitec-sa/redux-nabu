c6dfe541dbbb4b170be4cbba4a11294ef93643ed
Jonny Quarta <quarta@epsitec.ch>
2016-10-07 11:18:36 +0200

    WIP: change initial state structure

--- src/initial-state.js
+++ src/initial-state.js
@@ -5,22 +5,43 @@ const {fromJS} = require ('immutable');
 const DEFAULT_LOCALE = 'en-US';
 
 const state = fromJS ({
-  defaultLocale: DEFAULT_LOCALE,
-  locale:  DEFAULT_LOCALE,
+  selectedLocale:  DEFAULT_LOCALE,
+  locales: [],
+
   nabuGen: 0,
   marker:  false,
   focus:   null,
+
   selectionMode: {
     enabled: false,
     selectedItemId: null,
   },
-  translator: fromJS ({
+
+  translator: {
     isOpen: false,
     tableSize: 0
-  }),
-  translations: fromJS ({
-    [DEFAULT_LOCALE]: fromJS ({}),
-  })
+  },
+
+  messages: []
 });
 
 module.exports = state;
+
+
+/*
+Message is of type:
+
+[id]: {
+  defaultMessage: [id],
+  description: '',
+  translations: {
+    [locale1]: {
+      message: ''
+    }
+  }
+}
+
+
+
+
+*/
